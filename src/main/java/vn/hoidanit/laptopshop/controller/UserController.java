756983a26fd7006b7b85d9508fb2688983c191de
“nguyenvailon” <nguyendeptrai812@gmail.com>
2025-08-02 00:46:54 +0700

    Update 1/8

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,32 +1,39 @@
 package vn.hoidanit.laptopshop.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//     public String getHomePage() {
-
-//         @RequestMapping("/")
-
-//         return "Hello from controller";
-//     }
-
-// }
-@RestController
+@Controller
 public class UserController {
+    // // DI: Dependency Injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("/hoidanit")
+
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 
 }
+// @RestController
+// public class UserController {
+// // DI: Dependency Injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
