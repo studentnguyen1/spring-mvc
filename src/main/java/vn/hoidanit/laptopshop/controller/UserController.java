8cd24178f0b9da6e9d01ec495b43eef7b5903004
“nguyenvailon” <nguyendeptrai812@gmail.com>
2025-08-08 02:10:02 +0700

    Update 8/8

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,8 +2,11 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -19,23 +22,27 @@ public class UserController {
 
     public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        String controlService = "Hello from Controller";
         model.addAttribute("test", test);
+        model.addAttribute("controller", controlService);
         return "hello";
     }
 
+    @RequestMapping("/admin/user")
+
+    public String getCreateUser(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println(hoidanit);
+        String test1 = "Hello Create ADmin";
+        return "hello";
+
+    }
+
 }
-// @RestController
-// public class UserController {
-// // DI: Dependency Injection
-// private UserService userService;
-
-// public UserController(UserService userService) {
-// this.userService = userService;
-// }
-
-// @GetMapping("")
-// public String getHomePage() {
-// return this.userService.handleHello();
-// }
-
-// }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -1,17 +1,32 @@
-<html lang="en">
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+        <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <!-- Latest compiled and minified CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<body>
-    hello from jsp
-    <h1>
-        ${test}
-    </h1>
+            <!-- Latest compiled JavaScript -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
-</body>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+            <!-- <link href="/css/demo.css" rel="stylesheet"> -->
 
-</html>
+        </head>
+
+        <body>
+            hello from jsp
+            <h1>
+                ${test}
+            </h1>
+            <h2>
+                ${controller}
+            </h2>
+            <button class="btn btn-warning">Submit</button>
+
+        </body>
+
+        </html>
