b00cba1211fd8ede0a34d5e3794fb2300a461112
“nguyenvailon” <nguyendeptrai812@gmail.com>
2025-10-05 10:12:21 +0700

    Update 05/10

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -13,12 +13,14 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     void deleteById(long id);
 
-    List<User> findByEmail(String email);
+    // List<User> findByEmail(String email);
 
     List<User> findAll();
 
     User findById(long id);
 
     boolean existsByEmail(String email);
+
+    User findByEmail(String email);
 
 }
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -63,4 +63,8 @@ public class UserService {
         return this.userRepository.existsByEmail(email);
     }
 
+    public User getUserByEmail(String email) {
+        return this.userRepository.findByEmail(email);
+    }
+
 }
